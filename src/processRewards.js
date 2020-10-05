d5e0913ae723dcbe8bb70b2e34f25e8d718b0a57
YoungKidWarrior <ginelliunit@gmail.com>
2020-10-05 16:59:23 -0700

    Liquidity Provider 1

--- src/processRewards.js
+++ src/processRewards.js
@@ -1,24 +1,44 @@
 /* eslint-disable consistent-return */
 const fs = require('fs');
 const BigNumber = require('bignumber.js');
-const providers = require('../providers.json');
+let providers = require('../providers.json');
 const transaction = require('../config/dao.json');
 const { tokensToMint } = require('../config/schedule.json');
 
 function mintSettings(tx) {
-	const {
-		pair: { totalSupply }
+	let {
+		pair: { totalSupply },
 	} = providers[0];
+
+	const agent = providers.find((element) => element.user.address === '0x5cb045fd63f95c208048c38e0abd2cdb3537c68e');
+	if (agent) {
+		providers = providers.filter((element) => element.user.address !== agent.user.address);
+		totalSupply = totalSupply - agent.liquidityTokenBalance;
+	}
+
+	//Combine addresses with token balances
 	const providerAddresses = providers.map((element) => element.user.address);
 	const providerTokens = providers.map((element) => element.liquidityTokenBalance);
+
+	//Calculate rewards
 	const providerRewards = providerTokens.map((element) =>
-		new BigNumber(element).dividedBy(totalSupply).multipliedBy(tokensToMint).toFixed(18).toString().replace('.', '')
+		new BigNumber(element).dividedBy(totalSupply).multipliedBy(tokensToMint).toFixed(18).toString()
 	);
 	const providerMints = providerAddresses.map((element, i) => [element, providerRewards[i]]);
 
+	// Save to file marked today
+	const now = new Date().toISOString().split('T')[0];
+	fs.writeFile(`./log/rewards-${now}.json`, JSON.stringify(providerMints), (err) => {
+		if (err) {
+			console.log('Did not save transaction settings');
+			console.log(err);
+		}
+	});
+
+	//Cut decimals for transaction
+	providerMints.forEach((provider) => (provider[1] = provider[1].replace('.', '')));
 	const settings = tx;
 	settings[0].mints = providerMints;
-
 	return JSON.stringify(settings, null, 2);
 }
 
